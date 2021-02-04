c993edb47c961a78bf7bf63469520198aefa9573
Srikant V <sv092003@gmail.com>
2021-02-04 13:01:15 -0500

    add basic servo movements

--- dinosaurScript.ino
+++ dinosaurScript.ino
@@ -6,12 +6,13 @@ Servo right;
 void setup() 
 { 
   left.attach(9);
-  left.write(45);
+  left.write(45); // set servo to mid-point
   right.attach(10);
   right.write(45); // set servo to mid-point
 } 
 
 void loop() {
+  //write the motors in opposite directions
   left.write(60);
   right.write(30);
   delay(1000);
